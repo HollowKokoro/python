9d672caffab8416abf397ba0e75d2e1501ecd5ba
Cyril <kirill.lihotin@gmail.com>
2020-11-06 20:59:22 +0500

    update

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 a = 'type'
 b = 'brand'
 c = 'amount'
+d = 'manufacturer'
+e = 'price'
+f = 'size'
 database = []
 
 
-def addToBase(db, tpe, brand, amount):
-    good = {'Type': tpe, 'Brand': brand, 'Amount': amount}
+def addToBase(db, tpe, brand, amount, manufacturer, price, size):
+    good = {a: tpe, b: brand, c: amount, d: manufacturer, e: price, f: size}
     db.append(good)
 
 while 1:
@@ -15,5 +18,11 @@ while 1:
     brand = input()
     print('Введите количество товара:')
     amount = input()
-    addToBase(database, tpe, brand, amount)
+    print('Введите производителя:')
+    manufacturer = input()
+    print('Введите цену:')
+    price = input()
+    print('Введите размер:')
+    size = input()
+    addToBase(database, tpe, brand, amount, manufacturer, price, size)
     print(database)
