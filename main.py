d2122818e793c803c5723587d979ab3166c745c4
Cyril <kirill.lihotin@gmail.com>
2020-11-06 20:44:12 +0500

    update

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-a = 'good'
-b = 'name'
+a = 'type'
+b = 'brand'
 c = 'amount'
-database = {}
+database = []
 
-def addToDict():
-    good1 = {a: 'Кроссовки', b: 'Nike', c: 25}
-    database = {good1}
 
-while True:
-    addToDict()
+def addToBase(db, tpe, brand, amount):
+    good = {'Type': tpe, 'Brand': brand, 'Amount': amount}
+    db.append(good)
+
+while 1:
+    print('Введите тип товара:')
+    tpe = input()
+    print('Введите бренд товара:')
+    brand = input()
+    print('Введите количество товара:')
+    amount = input()
+    addToBase(database, tpe, brand, amount)
     print(database)
